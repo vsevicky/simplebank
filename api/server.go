cf340d7f9adcb5ac27c7e59c51c4b978b15efec8
Vicky Kumar <99803613+vsevicky@users.noreply.github.com>
2024-03-03 13:36:25 +0530

    addign account transfer api

--- api/server.go
+++ api/server.go
@@ -20,6 +20,8 @@ func NewServer(store db.Store) *Server {
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
 
+	router.POST("/transfers", server.createTransfer)
+
 	// Add routes to the router
 	server.router = router
 	return server
